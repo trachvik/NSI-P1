1f30c4eed8ee8fb741c6b046775f1d541134e553
trachvik <vitrachta@gmail.com>
2026-05-08 18:37:21 +0200

    State led

--- main_rpi.py
+++ main_rpi.py
@@ -7,9 +7,17 @@ poll = uselect.poll()
 poll.register(sys.stdin, uselect.POLLIN)
 
 sensor = dht.DHT22(machine.Pin(20))
-timer = machine.Timer()
+
+timer_data = machine.Timer()
+timer_disconnected = machine.Timer()
+timer_blinks = machine.Timer()
+timer_connected = machine.Timer()
+blink_counter = 0
+
 flag = False
 init = True
+
+state_led = machine.Pin('LED', machine.Pin.OUT)  # Onboard LED
 
 led_r = machine.PWM(machine.Pin(16))
 led_r.freq(5000)
@@ -18,7 +26,29 @@ led_g.freq(5000)
 led_b = machine.PWM(machine.Pin(18))
 led_b.freq(5000)
 
-def measure_flag(timer):
+def blink_cb(t):
+    global blink_counter
+    blink_counter += 1
+    if blink_counter <= 3:
+        state_led.value(1)
+        timer_blinks.init(period=100, mode=machine.Timer.ONE_SHOT, callback=blink_off_cb)
+    else:
+        blink_counter = 0
+
+def blink_off_cb(t):
+    state_led.value(0)
+    timer_blinks.init(period=100, mode=machine.Timer.ONE_SHOT, callback=blink_cb)
+
+def disconnected_clb(t):
+    global blink_counter
+    blink_counter = 0
+    blink_cb(t)
+
+def data_led_off(t):
+    state_led.value(0)
+
+
+def measure_flag(t):
     global flag
     flag = True
 
@@ -28,15 +58,16 @@ def handshake():
         if data.strip() == '<PING>':
             sys.stdout.write('<PONG:PICO_OK>\n') # sys.stdout.write() receives a string and sends UTF-8 encoded bytes to the serial port
             print('RPI:Handshake successful')
+            timer_disconnected.deinit()  # stop disconnect blinking when connected
             break
 
 def parse_n_handle(data):
     global init
     if data.startswith('<SET_T:'):
         init = False
-        #print(f'RPI: Received command to set measurement period: {data.strip()} ms')
+        state_led.value(0)
         measure_period = int(data.strip()[7:-1])  # Extract the number from the command | [7:-1] removes the '<SET_T:' prefix (7) and the '>' suffix ()
-        timer.init(period=measure_period, mode=machine.Timer.PERIODIC, callback=measure_flag)
+        timer_data.init(period=measure_period, mode=machine.Timer.PERIODIC, callback=measure_flag)
     elif data.startswith('<LED:'):
         #print(f'RPI: Received command to set LED color: {data.strip()}')
         color_values = data.strip()[5:-1].split(',')  # Extract the RGB values from the command | [5:-1] removes the '<LED:' prefix (5) and the '>' suffix ()
@@ -44,6 +75,9 @@ def parse_n_handle(data):
         led_r.duty_u16(int(r * 65535 / 255))  # Scale 0-255 to 0-65535 for PWM duty cycle
         led_g.duty_u16(int(g * 65535 / 255))
         led_b.duty_u16(int(b * 65535 / 255))
+        
+
+timer_disconnected.init(period=5000, mode=machine.Timer.PERIODIC, callback=disconnected_clb)
 
 handshake()
 
@@ -57,4 +91,6 @@ while True:
         temp = sensor.temperature()
         sys.stdout.write(f'<DATA:{temp:.1f}>\n')
         #print(f'RPI: Sent data to server: {temp:.1f} °C')
+        state_led.value(1)
+        timer_connected.init(period=50, mode=machine.Timer.ONE_SHOT, callback=data_led_off)
         flag = False
